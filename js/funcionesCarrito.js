67983f9b0bd40b82f2904f659c6ff9f509627298
Silvia Sosa Visuara <silvia_sosa@hotmail.com>
2020-10-21 19:04:36 -0300

    commiteando en master

--- js/funcionesCarrito.js
+++ js/funcionesCarrito.js
@@ -42,12 +42,12 @@ function cargaInicialDatos() {
 	agregarUsuario(lucas);
 	agregarUsuario(franco);
 	// ---------- [Creacion de Productos (por instancias de objetos)] ---------- //
-	let prod1 = new Producto(1, "prod uno", "ideal para primavera verano", "M", "", 750, 3, ["frio" ]);
-	let prod2 = new Producto(2, "prod dos", "ideal para primavera verano", "G", "", 1000, 4, ["calor" ]);
-	let prod3 = new Producto(3, "prod tres",  "ideal para primavera verano", "U", "", 1999,99, 3, ["calor"]);
-	let prod4 = new Producto(4, "prod cuatro","ideal para primavera verano", "P", "", 999,9, 3, ["calor"]);
-	let prod5 = new Producto(5, "prod cinco", "ideal para primavera verano", "M", "", 799,99, 1, ["todos_los_dias" ]);
-	let prod6 = new Producto(6, "prod seis","ideal para primavera verano", "M", "", 850, 3, ["frío" ]);
+	let prod1 = new Producto(1, "prod uno", "ideal para primavera verano", "M", "http://2.gravatar.com/avatar/be47344067db1d41b3e366b5877b558d", 750, 3, ["frio" ]);
+	let prod2 = new Producto(2, "prod dos", "ideal para primavera verano", "G", "http://2.gravatar.com/avatar/be47344067db1d41b3e366b5877b558d", 1000, 4, ["calor" ]);
+	let prod3 = new Producto(3, "prod tres",  "ideal para primavera verano", "U", "http://2.gravatar.com/avatar/be47344067db1d41b3e366b5877b558d", 1999,99, 3, ["calor"]);
+	let prod4 = new Producto(4, "prod cuatro","ideal para primavera verano", "P", "http://2.gravatar.com/avatar/be47344067db1d41b3e366b5877b558d", 999,9, 3, ["calor"]);
+	let prod5 = new Producto(5, "prod cinco", "ideal para primavera verano", "M", "http://2.gravatar.com/avatar/be47344067db1d41b3e366b5877b558d", 799,99, 1, ["todos_los_dias" ]);
+	let prod6 = new Producto(6, "prod seis","ideal para primavera verano", "M", "http://2.gravatar.com/avatar/be47344067db1d41b3e366b5877b558d", 850, 3, ["frío" ]);
 	// -------- [Agregamos Productos a la BD (por instancias de objetos)] -------- //
 	agregarProducto(prod1);
 	agregarProducto(prod2);
