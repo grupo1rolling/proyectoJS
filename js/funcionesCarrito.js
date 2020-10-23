af00d175a1b18bb2fa7cf14d79d4644bb4c303ab
Silvia Sosa Visuara <silvia_sosa@hotmail.com>
2020-10-23 01:39:20 -0300

    index y funcionesCarrito modificados

--- js/funcionesCarrito.js
+++ js/funcionesCarrito.js
@@ -3,14 +3,16 @@ import { agregarProducto } from "./funcionesProductos.js";
 import { agregarUsuario } from "./funcionesUsuarios.js";
 
 // --------------------- [inicialización de variables] --------------------- //
+let total=document.getElementById("totalCarrito");
+let conta=document.getElementById("contador");
 let totalARSCarrito=0, contadorProdCarrito=0;
 let dbProductos=[];
 let arrayProdComprados=[];
-//let ItemCompra;
 let carrito=[];
-// -------------------- [inicialización de variables] ---------------------- //
+// ------------------------ [llamada a funciones] -------------------------- //
 cargaInicialDatos();
 getProductos();
+console.log(dbProductos);                                               // ### OJO QUE ES STRING ### //
 actualizarTotalesCarrito();
 mostrarTarjetas();
 
@@ -26,6 +28,7 @@ console.log(`valor autenticado ${autenticado} indica si un usuario esta logueado
 
 // ------------- [carga inicial de datos PRODUCTOS Y USUARIOS] ------------- //
 function cargaInicialDatos() {
+    localStorage.clear();
     // -------------- [inicializamos ADMINISTRADOR)] -------------- //
 	let admin = new Usuario(0, "Administrador", "Supremo","admin@naturecollection.com", "admin", [], [], true, true);
 	agregarUsuario(admin);
@@ -44,10 +47,10 @@ function cargaInicialDatos() {
 	// ---------- [Creacion de Productos (por instancias de objetos)] ---------- //
 
 	let prod1 = new Producto(1, "prod uno", "ideal para primavera verano", "M", "https://via.placeholder.com/150/f66b97", 750, 3,"frio");
-	let prod2 = new Producto(2, "prod dos", "ideal para primavera verano", "G", "https://via.placeholder.com/150/24f355", 1000, 4, "calor");
-	let prod3 = new Producto(3, "prod tres",  "ideal para primavera verano", "U", "https://via.placeholder.com/150/771796", 1999,99, 3, "calor");
-	let prod4 = new Producto(4, "prod cuatro","ideal para primavera verano", "P", "https://via.placeholder.com/150/92c952", 999,9, 3, "calor");
-	let prod5 = new Producto(5, "prod cinco", "ideal para primavera verano", "M", "https://via.placeholder.com/150/f66b97", 799,99, 1, "todos_los_dias");
+	let prod2 = new Producto(2, "prod dos", "ideal para primavera verano", "G", "https://via.placeholder.com/150/24f355", 1000, 2, "calor");
+	let prod3 = new Producto(3, "prod tres",  "ideal para primavera verano", "U", "https://via.placeholder.com/150/771796", 1810, 3, "calor");
+	let prod4 = new Producto(4, "prod cuatro","ideal para primavera verano", "P", "https://via.placeholder.com/150/92c952", 999, 3, "calor");
+	let prod5 = new Producto(5, "prod cinco", "ideal para primavera verano", "M", "https://via.placeholder.com/150/f66b97", 876, 0, "todos_los_dias");
 	let prod6 = new Producto(6, "prod seis","ideal para primavera verano", "M", "https://via.placeholder.com/150/771796", 850, 3, "frío");
 
 	// -------- [Agregamos Productos a la BD (por instancias de objetos)] -------- //
@@ -61,14 +64,15 @@ function cargaInicialDatos() {
 
 // -------------------- [actualiza totales del Carrito] -------------------- //
 function actualizarTotalesCarrito() {
-    document.getElementById("totalCarrito").innerHTML=totalARSCarrito.toFixed(2);
-    document.getElementById("contador").innerHTML=contadorProdCarrito;
+    console.log ("PASA POR ACTUALIZAR TOTALES");
+    total.innerHTML=totalARSCarrito.toFixed(2);
+    conta.innerHTML=contadorProdCarrito;
 }
 
-// -------------------- [actualiza totales del Carrito] -------------------- //
+// ----------------------- [funcion vaciarCarrito] ----------------------- //
 let botonVaciar = document.querySelector('#botonVaciar');
-// Eventos, en este caso escucho al boton vaciarCarrito del modal
 botonVaciar.addEventListener('click', vaciarCarrito);
+//
 function vaciarCarrito() {
     totalARSCarrito=0;
     contadorProdCarrito=0;
@@ -79,18 +83,19 @@ function vaciarCarrito() {
 
 // ----------------- [traemos productos del local Storage] ----------------- //          
 function getProductos() {
+    console.log("db productos ACTUALIZADOS");                //#### 4 TESTING PURPOSES ONLY ###
     dbProductos=JSON.parse(localStorage.getItem("productos")) || [];
  }
 
 // ---------------- [guardamos productos del local Storage] ---------------- //
-/*function setLocalStorageProd () {
-    localStorage.setItem('productos', JSON.stringify(dbProductos))
-}*/
+function setProductos () {
+    console.log("local storage productos ACTUALIZADOS");                //#### 4 TESTING PURPOSES ONLY ###
+    localStorage.setItem('productos', JSON.stringify(dbProductos));
+}
 
 
 // -------------------- [mostrar tarjetas dinámicamente] ------------------- //
 function mostrarTarjetas() {
-   
     let tarjProd = document.getElementById("tarjetasProd");
     dbProductos.map(function(prod, index){
         let tarjeta = `
@@ -106,91 +111,63 @@ function mostrarTarjetas() {
                   <p id="categoriaProducto">lala</p>
                 </div>
                 <div class="card-footer">
-                <a href="#" class="btn btn-green mt-3" onclick="comprarProd(${index})">Añadir al carro</a>
+                <a href="#" class="btn btn-green mt-3" id="botonComprar" onclick="comprarProd(${index})">Añadir al carro</a>
                 </div>
               </div>       
             `;
         tarjProd.innerHTML += tarjeta;
-        console.log (`se cargó la tarjeta ${index}`)
         });
 }
+// <a href="#" class="btn btn-green mt-3" id="botonComprar" onclick="comprarProd(${index})">Añadir al carro</a>
 
-// ------- [vamos a poner en un array los codigo productos comprados] ------ //
-
-//FUNCION COMPRAR PRODUCTO
-comprarProd(0);
-
+// -------------------- [funcion comprarProducto] -------------------- //
+let botonComprar = document.querySelector('#botonComprar');
+botonComprar.addEventListener('click', comprarProd);
+//
 function comprarProd(i) {
-
-    console.log (`VAMOS A COMPRAR ${i}`)
-    //busco el prod en el array productos
+    console.log (`VAMOS A COMPRAR ${i}`)                                //#### 4 TESTING PURPOSES ONLY ###
+    //busco el prod en la bdProductos
     let prod = dbProductos[i];
-    console.log(prod);
+    console.log(`EL PRODUCTO COMPRADO ------>  ${prod}`);               //#### 4 TESTING PURPOSES ONLY ###
 
-    //si hay al menos un prod entonces se puede vender 
+    //si hay al menos un producto en stock, se puede vender 
     if (prod._stock >= 1) {
-            console.log ("actualizamos stock")
-            dbProductos[i].stock -= 1;
-            arrayProdComprados.push(prod._codigo);
-            console.log (`arrayProdComprados ---> ${arrayProdComprados}`);
-            let idProd=prod._codigo;
-            let nomProd=prod._nombre;
-            let cantProd=1;
-            let precioProd=prod._precio;
+        console.log ("actualizamos stock")                          //#### 4 TESTING PURPOSES ONLY ###
+        dbProductos[i].stock -= 1;
+        setProductos();
+        getProductos();
+        //
+        arrayProdComprados.push(prod._codigo);
+        console.log (`arrayProdComprados ---> ${arrayProdComprados}`); //#### 4 TESTING PURPOSES ONLY ###
+        //
+        let idProd=prod._codigo;
+        let nomProd=prod._nombre;
+        let cantProd=1;
+        let precioProd=prod._precio;
+        let itemCompra = new ItemCarrito(idProd, nomProd, cantProd, precioProd);
+        
+        itemCompra._idProd=idProd;
+        itemCompra._nomProd=nomProd;
+        itemCompra._cantProd=cantProd;
+        itemCompra._precioProd=precioProd;
+        console.log (itemCompra);                                  //#### 4 TESTING PURPOSES ONLY ###
+        
+        carrito.push(itemCompra);
+        alert('Se ha añadido un item al carrito')                   //#### 4 TESTING PURPOSES ONLY ###
 
-            let itemCompra = new ItemCarrito(idProd, nomProd, cantProd, precioProd);
-            
-            itemCompra._idProd=idProd;
-            itemCompra._nomProd=nomProd;
-            itemCompra._cantProd=cantProd;
-            itemCompra._precioProd=precioProd;
-            console.log ("itemCompra");
-            
-            carrito.push(itemCompra);
-            console.log('Se ha añadido un item al carrito')
 
+        //en totalCompra sumo los importes de los prod comprados
+        totalARSCarrito += prod._precio;
+        contadorProdCarrito += 1;
+        actualizarTotalesCarrito;
 
-            //en totalCompra sumo los importes de los prod comprados
-            totalARSCarrito += prod._precio;
-            contadorProdCarrito += prod._precio;
-            console.log(totalARSCarrito);
-            console.log(contadorProdCarrito);
-            actualizarTotalesCarrito;
+        console.log(`TOTAL CARRO --->${totalARSCarrito}`);             //#### 4 TESTING PURPOSES ONLY ###
+        console.log(`CONTADOR ---->${contadorProdCarrito}`);          //#### 4 TESTING PURPOSES ONLY ###
     } else {
-        console.error(`ooops nos quedamos sin ${prod._nombre}`);
+            alert(`ooops nos quedamos sin ${prod._nombre}`);
     }
 }
-
-        
-//         //actualizo la cantidad del producto vendido en el array de productos
-//         let indice = productos.findIndex(item => {
-//             return item.id == i
-//         })
-//         productos[indice].cantidad -= 1
-
-//         console.log('Se ha añadido un item al carrito')
-
-//     } else {
-//         
-//         listarStock()
-//  
-
-
-// ----------- [pasamos del array de compra a items del carrito] ----------- //
-/*
-function agregarAlCarrito(cod) {
-    console.log("VAMOS A COMPLETAR LAS LINEAS DEL CARRITO ")
-
-// -------------------- [creamos INSTANCIA ItemCarrito] -------------------- //
-let itemCarrito= new ItemCarrito (idProd, nomProd, cantProd, precioProd);
-// let idP,nomP,cantP, precioP;
-// //inicializamos INSTANCIA ItemCarrito
-// itemCart.idProd = idP;
-// itemCart.nomProd = nomP;
-// itemCart.cantProd= cantP;
-// itemCart.precioProd = precioP;
-}
-*/
+   
 
 
 // -------------------------- [finalizar compra] --------------------------- //
