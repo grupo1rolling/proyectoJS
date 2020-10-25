f91163e3e07a14800948b2b4d83c4cbb66f163f2
Ale <alejandro.alecarol@gmail.com>
2020-10-25 11:10:06 -0300

    Correccion de formato del codigo

--- js/funcionesCarrito.js
+++ js/funcionesCarrito.js
@@ -3,14 +3,14 @@ import { cargaInicialDatos } from "./funcionesAuxiliares.js";
 
 
 // --------------------- [inicialización de variables] --------------------- //
-let total=document.getElementById("totalCarrito");
-let conta=document.getElementById("contador");
-let totalARSCarrito=0, contadorProdCarrito=0;
-let dbProductos=[];
-let arrayProdComprados=[];
-let carrito=[];
-let mensajes=document.getElementById("mensajes");
-let texto="" ;
+let total = document.getElementById("totalCarrito");
+let conta = document.getElementById("contador");
+let totalARSCarrito = 0, contadorProdCarrito = 0;
+let dbProductos = [];
+let arrayProdComprados = [];
+let carrito = [];
+let mensajes = document.getElementById("mensajes");
+let texto = "";
 
 // ------------------------ [llamada a funciones] -------------------------- //
 cargaInicialDatos();
@@ -24,13 +24,13 @@ mostrarTarjetas();
 localStorage.setItem("usuarioAutenticado", true);
 let autenticado = localStorage.getItem("usuarioAutenticado");          // ### OJO QUE ES STRING ### //
 console.log(`valor autenticado ${autenticado} indica si un usuario esta logueado `);
-mensajes.innerHTML="bienvenido"
+mensajes.innerHTML = "bienvenido"
 
 
 // --- ###################### [F U N C I O N E S] ###################### --- //
 
 // -------------------- [actualiza totales del Carrito] -------------------- //
-function actualizarTotalesCarrito() { 
+function actualizarTotalesCarrito() {
 	total.innerHTML = totalARSCarrito.toFixed(2);
 	conta.innerHTML = contadorProdCarrito;
 } // FIN [actualiza totales del Carrito]
@@ -39,16 +39,16 @@ function actualizarTotalesCarrito() {
 let botonVaciar = document.querySelector('#botonVaciar');
 botonVaciar.addEventListener('click', vaciarCarrito);
 function vaciarCarrito() {
-    if (confirm("estas seguro?")) {
-        totalARSCarrito=0;
-        contadorProdCarrito=0;
-        arrayProdComprados=[];
-        carrito=[];
-        actualizarTotalesCarrito();
-        mensajes.innerHTML="carrito vacío"                 //#### 4 TESTING PURPOSES ONLY ###
-    } else {
-        mensajes.innerHTML="sigue comprando"                 //#### 4 TESTING PURPOSES ONLY ###
-    }
+	if (confirm("estas seguro?")) {
+		totalARSCarrito = 0;
+		contadorProdCarrito = 0;
+		arrayProdComprados = [];
+		carrito = [];
+		actualizarTotalesCarrito();
+		mensajes.innerHTML = "carrito vacío"                 //#### 4 TESTING PURPOSES ONLY ###
+	} else {
+		mensajes.innerHTML = "sigue comprando"                 //#### 4 TESTING PURPOSES ONLY ###
+	}
 }; // FIN [funcion vaciarCarrito]
 
 
@@ -59,9 +59,9 @@ function getProductos() {
 } // FIN [traemos dbProductos del localStorage]
 
 // ----------------- [guardamos productos del localStorage] ---------------- //
-function setProductos () {
-    console.log("localStorage productos ACTUALIZADOS");                 //#### 4 TESTING PURPOSES ONLY ###
-    localStorage.setItem('productos', JSON.stringify(dbProductos));
+function setProductos() {
+	console.log("localStorage productos ACTUALIZADOS");                 //#### 4 TESTING PURPOSES ONLY ###
+	localStorage.setItem('productos', JSON.stringify(dbProductos));
 } // FIN [guardamos productos del localStorage]
 
 
@@ -94,98 +94,98 @@ function mostrarTarjetas() {
 function agregarCarrito(producto) {
 	let productosAgregados = [];
 	//-- Verificamos si existe key='productosCarrito' en localStorage --//
-	productosAgregados = JSON.parse(localStorage.getItem("productosCarrito")) || [] ;
+	productosAgregados = JSON.parse(localStorage.getItem("productosCarrito")) || [];
 	productosAgregados.push(producto);
 	localStorage.setItem("productosCarrito", JSON.stringify(productosAgregados));
 } // FIN [agregarCarrito]
 
 // ----------------- [traemos Carrito del localStorage] ----------------- //          
 function getCarrito() {
-    carrito=JSON.parse(localStorage.getItem("productosCarrito")) || [];
-    console.log("paso por getCarrito");                                 //#### 4 TESTING PURPOSES ONLY ###
-    console.log(carrito);                                               //#### 4 TESTING PURPOSES ONLY ###
- } // FIN [traemos Carrito del localStorage] 
+	carrito = JSON.parse(localStorage.getItem("productosCarrito")) || [];
+	console.log("paso por getCarrito");                                 //#### 4 TESTING PURPOSES ONLY ###
+	console.log(carrito);                                               //#### 4 TESTING PURPOSES ONLY ###
+} // FIN [traemos Carrito del localStorage] 
 
 // ---------------- [guardamos Carrito al localStorage] ---------------- //
 function setCarrito() {
-    localStorage.setItem('productosCarrito', JSON.stringify(carrito));
+	localStorage.setItem('productosCarrito', JSON.stringify(carrito));
 } // FIN [guardamos Carrito al localStorage]
 
 // -------------------- [funcion comprarProducto] -------------------- //
-window.comprarProd= function (i) {
-    
-    //busco el prod en la bdProductos por posicion
-    let prod = dbProductos[i];
-                       
-    //si hay al menos un producto en stock, se puede vender 
-    if (prod._stock >= 1) {
-        //actualizamos stock, dbProductos y localStorage
-        dbProductos[i]._stock -= 1;
-        setProductos();
-        getProductos();
-        //
-        arrayProdComprados.push(prod._codigo);
-        console.log (`arrayProdComprados ---> ${arrayProdComprados}`); //#### 4 TESTING PURPOSES ONLY ###
-        //
-        let idProd=prod._codigo;
-        let nomProd=prod._nombre;
-        let cantProd=1;
-        let precioProd=prod._precio;
-        let itemCompra = new ItemCarrito(idProd, nomProd, cantProd, precioProd);
-        
-        itemCompra._idProd=idProd;
-        itemCompra._nomProd=nomProd;
-        itemCompra._cantProd=cantProd;
-        itemCompra._precioProd=precioProd;
+window.comprarProd = function (i) {
 
-        agregarCarrito(itemCompra);
-        getCarrito();
-        alert(`Se ha añadido un producto al carrito`)   //#### 4 TESTING PURPOSES ONLY ###
+	//busco el prod en la bdProductos por posicion
+	let prod = dbProductos[i];
 
-        // -- suma de precios y cantidad de productos comprados --/
-        totalARSCarrito += prod._precio;
-        contadorProdCarrito += 1;
-        actualizarTotalesCarrito() ;
+	//si hay al menos un producto en stock, se puede vender 
+	if (prod._stock >= 1) {
+		//actualizamos stock, dbProductos y localStorage
+		dbProductos[i]._stock -= 1;
+		setProductos();
+		getProductos();
+		//
+		arrayProdComprados.push(prod._codigo);
+		console.log(`arrayProdComprados ---> ${arrayProdComprados}`); //#### 4 TESTING PURPOSES ONLY ###
+		//
+		let idProd = prod._codigo;
+		let nomProd = prod._nombre;
+		let cantProd = 1;
+		let precioProd = prod._precio;
+		let itemCompra = new ItemCarrito(idProd, nomProd, cantProd, precioProd);
 
-    } else {
-            alert(`ooops nos quedamos sin ${prod._nombre}`);
-    }
+		itemCompra._idProd = idProd;
+		itemCompra._nomProd = nomProd;
+		itemCompra._cantProd = cantProd;
+		itemCompra._precioProd = precioProd;
+
+		agregarCarrito(itemCompra);
+		getCarrito();
+		alert(`Se ha añadido un producto al carrito`)   //#### 4 TESTING PURPOSES ONLY ###
+
+		// -- suma de precios y cantidad de productos comprados --/
+		totalARSCarrito += prod._precio;
+		contadorProdCarrito += 1;
+		actualizarTotalesCarrito();
+
+	} else {
+		alert(`ooops nos quedamos sin ${prod._nombre}`);
+	}
 } // FIN [funcion comprarProducto]
 
 
 // -------------------------- [finalizar compra] --------------------------- //
 let botonFinalizar = document.querySelector('#botonFinalizar');
-botonFinalizar.addEventListener('click', finalizarCompra);   
- function finalizarCompra() {
-    actualizarTotalesCarrito();
-    //alert (`Compra finalizada. ${contadorProdCarrito} prendas. Total a pagar $ ${totalARSCarrito}`);
-    if (autenticado=="true") {
-        alert("usuario autenticado");
-    } else {
-        alert ("usuario NO autenticado");
-    }
-    
-    // -- actualizar usuarios.odigosProductos[]  con arrayProdComprados --/
-    // llamar a getUsuario(cod) 
+botonFinalizar.addEventListener('click', finalizarCompra);
+function finalizarCompra() {
+	actualizarTotalesCarrito();
+	//alert (`Compra finalizada. ${contadorProdCarrito} prendas. Total a pagar $ ${totalARSCarrito}`);
+	if (autenticado == "true") {
+		alert("usuario autenticado");
+	} else {
+		alert("usuario NO autenticado");
+	}
+
+	// -- actualizar usuarios.odigosProductos[]  con arrayProdComprados --/
+	// llamar a getUsuario(cod) 
 } // FIN [finalizar compra]
 
 
 // ------------------- [buscar texto en dbProductos] ------------------- //
-texto=document.getElementById("texto").value
-window.buscarTexto = function() {
-    getProductos();
-    mensajes.innerHTML= `buscando ${texto}....`;
-    
-    let productosTexto = dbProductos.filter (p => {
-        return ((p._categoria.includes(texto) || p._nombre.includes(texto)) || p._descripcion.includes(texto))
-    });
-    console.log(productosTexto);                                          //#### 4 TESTING PURPOSES ONLY ###
-    
-    let tarjetasProd = document.getElementById("tarjetasProd");
-    
-    if (productosTexto != []) {
-        productosTexto.map(function (prod, index) {
-            let tarjeta = `
+texto = document.getElementById("texto").value
+window.buscarTexto = function () {
+	getProductos();
+	mensajes.innerHTML = `buscando ${texto}....`;
+
+	let productosTexto = dbProductos.filter(p => {
+		return ((p._categoria.includes(texto) || p._nombre.includes(texto)) || p._descripcion.includes(texto))
+	});
+	console.log(productosTexto);                                          //#### 4 TESTING PURPOSES ONLY ###
+
+	let tarjetasProd = document.getElementById("tarjetasProd");
+
+	if (productosTexto != []) {
+		productosTexto.map(function (prod, index) {
+			let tarjeta = `
                 <div class="card">
                 <img id="fotoProducto" src=${prod._foto} class="card-img-top" alt="top-estampa-cactus">
                 <div class="card-body">
@@ -202,31 +202,31 @@ window.buscarTexto = function() {
                 </div>
               </div>       
             `;
-            tarjetasProd.innerHTML += tarjeta;
-            });
-        mensajes.innerHTML= "se encontraron los siguientes productos ";
-        } else {
-            mensajes.innerHTML= "no se encontraron productos con el texto buscado ";  
-        }
-    document.getElementById("texto").value="";    
+			tarjetasProd.innerHTML += tarjeta;
+		});
+		mensajes.innerHTML = "se encontraron los siguientes productos ";
+	} else {
+		mensajes.innerHTML = "no se encontraron productos con el texto buscado ";
+	}
+	document.getElementById("texto").value = "";
 } // FIN [buscar texto en dbProductos]
 
 
 // ------------------- [filtrar productos por categoria] ------------------- //
-window.filtrarProductos = function(cat) {
-    getProductos();
-    mensajes.innerHTML= `buscando ${cat}....`;
-    
-    let productosXcat = dbProductos.filter (p=> {
-          return p._categoria === cat
-    });
-    console.log(productosXcat);                                          //#### 4 TESTING PURPOSES ONLY ###
+window.filtrarProductos = function (cat) {
+	getProductos();
+	mensajes.innerHTML = `buscando ${cat}....`;
 
-    let tarjetasProd = document.getElementById("tarjetasProd");
-    if (productosXcat.length > 0) {
-       
-        productosXcat.map(function (prod, index) {
-            let tarjeta = `
+	let productosXcat = dbProductos.filter(p => {
+		return p._categoria === cat
+	});
+	console.log(productosXcat);                                          //#### 4 TESTING PURPOSES ONLY ###
+
+	let tarjetasProd = document.getElementById("tarjetasProd");
+	if (productosXcat.length > 0) {
+
+		productosXcat.map(function (prod, index) {
+			let tarjeta = `
                 <div class="card">
                 <img id="fotoProducto" src=${prod._foto} class="card-img-top" alt="top-estampa-cactus">
                 <div class="card-body">
@@ -243,12 +243,12 @@ window.filtrarProductos = function(cat) {
                 </div>
               </div>       
             `;
-            tarjetasProd.innerHTML += tarjeta;
-            });
-            mensajes.innerHTML= "se encontraron los siguientes productos para la categoría ";  
-        } else {
-            mensajes.innerHTML= "no se encontraron productos para la categoría ";  
-        }
+			tarjetasProd.innerHTML += tarjeta;
+		});
+		mensajes.innerHTML = "se encontraron los siguientes productos para la categoría ";
+	} else {
+		mensajes.innerHTML = "no se encontraron productos para la categoría ";
+	}
 } // FIN [filtrar productos por categoria]
 
 
@@ -257,10 +257,10 @@ const btnListarCarrito = document.querySelector('#listarCarrito');
 btnListarCarrito.addEventListener('click', listarCarrito);
 
 function listarCarrito() {
-    if (carrito.length > 0)  {
-        let items = document.getElementById("items");
-        carrito.map (function (p,i) {
-        let linea = ` 
+	if (carrito.length > 0) {
+		let items = document.getElementById("items");
+		carrito.map(function (p, i) {
+			let linea = ` 
         <tr>
             <td scope="row" id="fila">${i + 1}</td>
             <td class="text-center">${p._cantProd}</td>
@@ -274,50 +274,49 @@ function listarCarrito() {
                 </i>
                 </button>
             </td>
-        </tr>`   
-        items.innerHTML += linea;
-        });
-        let t=document.getElementById("totales");
-        t.innerHTML=totalARSCarrito.toFixed(2);
-    } else {
-        alert ("carrito vacio");
-    }   
+        </tr>`
+			items.innerHTML += linea;
+		});
+		let t = document.getElementById("totales");
+		t.innerHTML = totalARSCarrito.toFixed(2);
+	} else {
+		alert("carrito vacio");
+	}
 } // FIN [listar carrito] 
 
 
 
 // ------------------------ [borra item del carrito] ------------------------ //
- window.borrarItem = function (idP,i) {
-    let fila=document.getElementById("fila").childElementCount;
-    
-    // borramos la fila con el producto que quitamos
-    let elemTabla = document.getElementById('items');
-    let tablaFila = elemTabla.getElementsByTagName('tr');
-    elemTabla.removeChild(tablaFila[fila]);
-    //
-    getCarrito();
-    //
-    let prodBorrar = carrito.find(itemC => {
-       return itemC._idProd === idP
-    });
-    console.log(`"voy a borrar:"   ${prodBorrar._idProd}`)
+window.borrarItem = function (idP, i) {
+	let fila = document.getElementById("fila").childElementCount;
 
-    
-    // -- [actualizamos los totales del Carrito] --/
-    totalARSCarrito-=prodBorrar._precioProd;
-    contadorProdCarrito-=1;
-    actualizarTotalesCarrito();
-    // -- [lo quitamos del arreglo de productos comprados y del carrito] --/
-    arrayProdComprados.splice(i,1);
-    carrito.splice(i,1);   
-    setCarrito();
+	// borramos la fila con el producto que quitamos
+	let elemTabla = document.getElementById('items');
+	let tablaFila = elemTabla.getElementsByTagName('tr');
+	elemTabla.removeChild(tablaFila[fila]);
+	//
+	getCarrito();
+	//
+	let prodBorrar = carrito.find(itemC => {
+		return itemC._idProd === idP
+	});
+	console.log(`"voy a borrar:"   ${prodBorrar._idProd}`)
 
-    // -- [actualizamos dbProductos] --/
-    getProductos();
-    let indice = dbProductos.findIndex (itemP => {
-        return itemP._codigo === idP
-    });
-    dbProductos[indice]._stock +=1;
-    setProductos();
+
+	// -- [actualizamos los totales del Carrito] --/
+	totalARSCarrito -= prodBorrar._precioProd;
+	contadorProdCarrito -= 1;
+	actualizarTotalesCarrito();
+	// -- [lo quitamos del arreglo de productos comprados y del carrito] --/
+	arrayProdComprados.splice(i, 1);
+	carrito.splice(i, 1);
+	setCarrito();
+
+	// -- [actualizamos dbProductos] --/
+	getProductos();
+	let indice = dbProductos.findIndex(itemP => {
+		return itemP._codigo === idP
+	});
+	dbProductos[indice]._stock += 1;
+	setProductos();
 } // FIN [borra item del carrito]
-    
