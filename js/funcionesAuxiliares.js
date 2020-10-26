369b24e22424501bada659a37a2b4165d280adbc
Ale <alejandro.alecarol@gmail.com>
2020-10-26 14:48:18 -0300

    -

--- js/funcionesAuxiliares.js
+++ js/funcionesAuxiliares.js
@@ -5,9 +5,9 @@ import { agregarUsuario, getAllUsuarios } from "./funcionesUsuarios.js";
 
 // ------------- [Carga inicial de datos USUARIOS Y PRODUCTOS] ------------- //
 export function cargaInicialDatos() {
-    localStorage.clear();
-    // -------------- [Inicializacion de ADMINISTRADOR)] -------------- //
-	let admin = new Usuario(0, "Administrador", "Supremo","admin@naturecollection.com", "admin", [], { dir1: "", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }, true, true);
+	localStorage.clear();
+	// -------------- [Inicializacion de ADMINISTRADOR)] -------------- //
+	let admin = new Usuario(0, "Administrador", "Supremo", "admin@naturecollection.com", "admin", [], { dir1: "", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }, true, true);
 	agregarUsuario(admin);
 
 	//---------------[Creacion de usuarios (Ejemplo)]---------------//
@@ -16,21 +16,21 @@ export function cargaInicialDatos() {
 	// let silvia = new Usuario(3, "SILVIA", "SOSA", "silvia@silvia", "lalala", [], [{ dir1: "San Juan 650", dir2: "", ciudad: "San Salvador de Jujuy", provincia: "Jujuy", codPostal: 4000 }], true, false);
 	// let lucas = new Usuario(4, "LUCAS", "RAMUNNI","lucas@lucas.com", "20565", [], [{ dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }], true, true);
 	// let franco = new Usuario(5, "FRANCO", "LEIRO","franco@franco.com", "59842", [1002, 1006, 1003], [{ dir1: "Av. Mate de Luna 750", dir2: "", ciudad: "Metam", provincia: "Salta", codPostal: 4000 }], true, false);
-	
-	let ale = new Usuario(1, "ALE", "CAROL","ale@ale.com", "12345", [1001, 1002], { dir1: "Av. Belgrano 2500", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }, true, false);
-	let mary = new Usuario(2, "MARY","BOSCH" ,"mary@mary.com", "65546", [1001, 1002, 1003, 1005, 1006], { dir1: "Marcos Paz 2500", dir2: "", ciudad: "Carlos Paz", provincia: "Córdoba", codPostal: 4000 }, true, true);
-	let silvia = new Usuario(3, "SILVIA", "SOSA", "silvia@silvia", "lalala", [], { dir1: "San Juan 650", dir2: "", ciudad: "San Salvador de Jujuy", provincia: "Jujuy", codPostal: 4000 }, true, false);
-	let lucas = new Usuario(4, "LUCAS", "RAMUNNI","lucas@lucas.com", "20565", [], { dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }, true, true);
-	let franco = new Usuario(5, "FRANCO", "LEIRO","franco@franco.com", "59842", [1002, 1006, 1003], { dir1: "Av. Mate de Luna 750", dir2: "", ciudad: "Metam", provincia: "Salta", codPostal: 4000 }, true, false);
-	
-// [{ dir1: "", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }]
-// [{ dir1: "Av. Belgrano 2500", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }]
-// [{ dir1: "Marcos Paz 2500", dir2: "", ciudad: "Carlos Paz", provincia: "Córdoba", codPostal: 4000 }]
-// [{ dir1: "San Juan 650", dir2: "", ciudad: "San Salvador de Jujuy", provincia: "Jujuy", codPostal: 4000 }]
-// [{ dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }]
-// [{ dir1: "Av. Mate de Luna 750", dir2: "", ciudad: "Metam", provincia: "Salta", codPostal: 4000 }]
 
-    // -------- [Agregamos usuarios (por instancias de objetos)] -------- //
+	let ale = new Usuario(1, "ALE", "CAROL", "ale@ale.com", "12345", [1001, 1002], { dir1: "Av. Belgrano 2500", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }, true, false);
+	let mary = new Usuario(2, "MARY", "BOSCH", "mary@mary.com", "65546", [1001, 1002, 1003, 1005, 1006], { dir1: "Marcos Paz 2500", dir2: "", ciudad: "Carlos Paz", provincia: "Córdoba", codPostal: 4000 }, true, true);
+	let silvia = new Usuario(3, "SILVIA", "SOSA", "silvia@silvia", "lalala", [], { dir1: "San Juan 650", dir2: "", ciudad: "San Salvador de Jujuy", provincia: "Jujuy", codPostal: 4000 }, true, false);
+	let lucas = new Usuario(4, "LUCAS", "RAMUNNI", "lucas@lucas.com", "20565", [], { dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }, true, true);
+	let franco = new Usuario(5, "FRANCO", "LEIRO", "franco@franco.com", "59842", [1002, 1006, 1003], { dir1: "Av. Mate de Luna 750", dir2: "", ciudad: "Metam", provincia: "Salta", codPostal: 4000 }, true, false);
+
+	// [{ dir1: "", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }]
+	// [{ dir1: "Av. Belgrano 2500", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }]
+	// [{ dir1: "Marcos Paz 2500", dir2: "", ciudad: "Carlos Paz", provincia: "Córdoba", codPostal: 4000 }]
+	// [{ dir1: "San Juan 650", dir2: "", ciudad: "San Salvador de Jujuy", provincia: "Jujuy", codPostal: 4000 }]
+	// [{ dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }]
+	// [{ dir1: "Av. Mate de Luna 750", dir2: "", ciudad: "Metam", provincia: "Salta", codPostal: 4000 }]
+
+	// -------- [Agregamos usuarios (por instancias de objetos)] -------- //
 	agregarUsuario(ale);
 	agregarUsuario(mary);
 	agregarUsuario(silvia);
@@ -38,21 +38,21 @@ export function cargaInicialDatos() {
 	agregarUsuario(franco);
 
 	// ---------- [Creacion de Productos (por instancias de objetos)] ---------- //
-	let prod1 = new Producto(1001, "Sweater", "Ideal para los días más fríos", "M", "https://via.placeholder.com/150/f66b97", 750, 2,"Frío");
+	let prod1 = new Producto(1001, "Sweater", "Ideal para los días más fríos", "M", "https://via.placeholder.com/150/f66b97", 750, 2, "Frío");
 	let prod2 = new Producto(1002, "Top", "Ideal para días más cálidos", "S", "https://via.placeholder.com/150/24f355", 1000, 2, "Calor");
-	let prod3 = new Producto(1003, "Short de baño",  "Para la piscina o la playa", "G", "https://via.placeholder.com/150/771796", 1810, 3, "Calor");
-	let prod4 = new Producto(1004, "Bolso extensible","Para todo lo que necesites llevar", "U", "https://via.placeholder.com/150/92c952", 999, 3, "Viaje");
+	let prod3 = new Producto(1003, "Short de baño", "Para la piscina o la playa", "G", "https://via.placeholder.com/150/771796", 1810, 3, "Calor");
+	let prod4 = new Producto(1004, "Bolso extensible", "Para todo lo que necesites llevar", "U", "https://via.placeholder.com/150/92c952", 999, 3, "Viaje");
 	let prod5 = new Producto(1005, "Jeans", "Jeans unisex negros", "M", "https://via.placeholder.com/150/f66b97", 876, 0, "Todos los días");
-	let prod6 = new Producto(1006, "Polar","Polar gris", "U", "https://via.placeholder.com/150/771796", 850, 3, "Frío");
+	let prod6 = new Producto(1006, "Polar", "Polar gris", "U", "https://via.placeholder.com/150/771796", 850, 3, "Frío");
 
 	// -------- [Agregamos Productos a la BD (por instancias de objetos)] -------- //
-    agregarProducto(prod1);
+	agregarProducto(prod1);
 	agregarProducto(prod2);
 	agregarProducto(prod3);
 	agregarProducto(prod4);
 	agregarProducto(prod5);
-    agregarProducto(prod6);
-	
+	agregarProducto(prod6);
+
 } // FIN [carga inicial de datos PRODUCTOS Y USUARIOS]
 
 export function getTalles() {
@@ -134,3 +134,17 @@ export function getProvincias() {
 		"Tucumán"
 	];
 }
+
+//VALIDACION DE CONTRASENIA
+export function validarPassword(pass) {
+	if (pass.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/)) {
+		console.log(`${pass}: True`);
+		return true;
+	} else {
+		alert("La contraseña no cumple las condiciones requeridas.\n\nDebe contener:\n♦ Entre 8 y 15 caracteres.\n♦ Al menos una letra mayúscula.\n♦ Al menos una letra minúscula.\n♦ Al menos un dígito.\n♦ No espacios en blanco.\n♦ Al menos un caracter especial.");
+		console.log(`${pass}: False`);
+		return false
+	}
+}
+
+
