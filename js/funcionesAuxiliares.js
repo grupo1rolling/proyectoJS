b3d982f1b7fc2b762410bd0a9506b3d4d6445bb5
Ale <alejandro.alecarol@gmail.com>
2020-10-26 17:03:10 -0300

    Elimino codigos innecesarios y de pruebas

--- js/funcionesAuxiliares.js
+++ js/funcionesAuxiliares.js
@@ -11,24 +11,11 @@ export function cargaInicialDatos() {
 	agregarUsuario(admin);
 
 	//---------------[Creacion de usuarios (Ejemplo)]---------------//
-	// let ale = new Usuario(1, "ALE", "CAROL","ale@ale.com", "12345", [1001, 1002], { dir1: "Av. Belgrano 2500", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }, true, false);
-	// let mary = new Usuario(2, "MARY","BOSCH" ,"mary@mary.com", "65546", [1001, 1002, 1003, 1005, 1006], [{ dir1: "Marcos Paz 2500", dir2: "", ciudad: "Carlos Paz", provincia: "Córdoba", codPostal: 4000 }], true, true);
-	// let silvia = new Usuario(3, "SILVIA", "SOSA", "silvia@silvia", "lalala", [], [{ dir1: "San Juan 650", dir2: "", ciudad: "San Salvador de Jujuy", provincia: "Jujuy", codPostal: 4000 }], true, false);
-	// let lucas = new Usuario(4, "LUCAS", "RAMUNNI","lucas@lucas.com", "20565", [], [{ dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }], true, true);
-	// let franco = new Usuario(5, "FRANCO", "LEIRO","franco@franco.com", "59842", [1002, 1006, 1003], [{ dir1: "Av. Mate de Luna 750", dir2: "", ciudad: "Metam", provincia: "Salta", codPostal: 4000 }], true, false);
-
 	let ale = new Usuario(1, "ALE", "CAROL", "ale@ale.com", "12345", [1001, 1002], { dir1: "Av. Belgrano 2500", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }, true, false);
-	let mary = new Usuario(2, "MARY", "BOSCH", "mary@mary.com", "65546", [1001, 1002, 1003, 1005, 1006], { dir1: "Marcos Paz 2500", dir2: "", ciudad: "Carlos Paz", provincia: "Córdoba", codPostal: 4000 }, true, true);
+	let mary = new Usuario(2, "MARY", "BOSCH", "mary@mary.com", "65546", [1001, 1002, 1003, 1005, 1006], { dir1: "Marcos Paz 2500", dir2: "", ciudad: "Carlos Paz", provincia: "Córdoba", codPostal: 4000 }, true, false);
 	let silvia = new Usuario(3, "SILVIA", "SOSA", "silvia@silvia", "lalala", [], { dir1: "San Juan 650", dir2: "", ciudad: "San Salvador de Jujuy", provincia: "Jujuy", codPostal: 4000 }, true, false);
-	let lucas = new Usuario(4, "LUCAS", "RAMUNNI", "lucas@lucas.com", "20565", [], { dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }, true, true);
+	let lucas = new Usuario(4, "LUCAS", "RAMUNNI", "lucas@lucas.com", "20565", [], { dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }, true, false);
 	let franco = new Usuario(5, "FRANCO", "LEIRO", "franco@franco.com", "59842", [1002, 1006, 1003], { dir1: "Av. Mate de Luna 750", dir2: "", ciudad: "Metam", provincia: "Salta", codPostal: 4000 }, true, false);
-
-	// [{ dir1: "", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }]
-	// [{ dir1: "Av. Belgrano 2500", dir2: "", ciudad: "San Miguel de Tucumán", provincia: "Tucumán", codPostal: 4000 }]
-	// [{ dir1: "Marcos Paz 2500", dir2: "", ciudad: "Carlos Paz", provincia: "Córdoba", codPostal: 4000 }]
-	// [{ dir1: "San Juan 650", dir2: "", ciudad: "San Salvador de Jujuy", provincia: "Jujuy", codPostal: 4000 }]
-	// [{ dir1: "Av. Roca 1100", dir2: "", ciudad: "La Plata", provincia: "Buenos Aires", codPostal: 4000 }]
-	// [{ dir1: "Av. Mate de Luna 750", dir2: "", ciudad: "Metam", provincia: "Salta", codPostal: 4000 }]
 
 	// -------- [Agregamos usuarios (por instancias de objetos)] -------- //
 	agregarUsuario(ale);
